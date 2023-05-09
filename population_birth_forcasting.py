1f8f51057b8f7ef929db972512e1d9122d15d662
mohdyasser42 <100227814+mohdyasser42@users.noreply.github.com>
2023-05-09 13:58:52 +0530

    Update population_birth_forcasting.py

--- population_birth_forcasting.py
+++ population_birth_forcasting.py
@@ -5,7 +5,7 @@ import warnings
 warnings.filterwarnings('ignore')
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearm.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import GridSearchCV
 
 def predict(country_name, year):
