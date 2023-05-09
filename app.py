c009e8fe24935fab4912430fd0595a5476c1a1f2
mohdyasser42 <100227814+mohdyasser42@users.noreply.github.com>
2023-05-09 12:51:30 +0530

    Add files via upload
    Major Project

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import json
+from streamlit_lottie import st_lottie
+from population_birth_forcasting import predict 
+
+st.set_page_config(page_title="Population and Birth Prediction",page_icon= "🤖",layout="wide")
+
+animation = st.container()
+heading = st.container()
+
+with heading:
+    st.markdown("<h1 style='text-align: center; margin: -2rem 0 1rem 0;'>Population and Birth Forcasting</h1>", unsafe_allow_html=True)
+
+def load_lottiefile(path:str):
+    with open(path,"r") as p:
+        return json.load(p)
+
+pop = load_lottiefile("./population.json")
+
+with animation:
+    st_lottie(pop, height= 250)
+
+with st.form("prediction_form"):
+    country = st.selectbox("Select country:",("India","Asia","World","South America", "Europe", "Africa" ,"Oceania" , "Australia", "United States of America", "Brazil","United Kingdom") )
+    year_input = st.text_input("Enter a year (YYYY):", max_chars=4, key="year_input")
+    submit_button = st.form_submit_button(label="Predict")
+
+st.write("---")
+
+if submit_button and year_input:
+    if not year_input.isnumeric():
+        st.write("Please enter a valid integer year.")
+    elif int(year_input) < 2022:
+        st.write("Please enter a valid year from 2023.")
+    elif int(year_input) > 2100:
+        st.write("Please enter a valid year less than 2100.")
+    else:
+        year = int(year_input)
+        prediction = predict(country,year)
+        st.write(f"The prediction from 2023 to {year} is:") 
+        st.write(prediction)
+        st.write("---")
+        st.markdown("<h2 style='text-align: center; margin-bottom: 1rem;'>Visualization</h2>", unsafe_allow_html=True)
+        with st.container():
+            left_column, right_column = st.columns(2)
+            with left_column:
+                st.write("Total Population Graph (in Thousands):")
+                st.bar_chart(prediction['Total Population'])
+                st.write("Gender Population Graph (in Thousands):")
+                st.bar_chart(prediction[['Male Population','Female Population',]])
+                st.write("Gender Ratio in Population Graph:")
+                st.bar_chart(prediction['Population Gender Ratio'])
+                st.write("Birth Rate Graph:")
+                st.bar_chart(prediction['Birth Rate'])
+
+
+            with right_column:       
+                st.write("Total Births Graph (in Thousands):")
+                st.bar_chart(prediction['Total Births'])
+                st.write("Gender Births Graph (in Thousands):")
+                st.bar_chart(prediction[['Male Births','Female Births']])
+                st.write("Gender Ratio in Births Graph:")
+                st.bar_chart(prediction['Birth Gender Ratio'])
+                
+
+        
---
+++ population_birth_forcasting.py
@@ -0,0 +1,131 @@
+# **Importing All The Neccessary Modules**
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import GridSearchCV
+
+def predict(country_name, year):
+    # DATA PREPARATION
+    # Converting the Dataset file into a pandas dataframe
+    df = pd.read_csv('WPP2022_Demographic_Indicators_Medium.csv')
+    # FEATURE SELECTION
+    Main_df = df[['Location', 'Time','TPopulation1July','TPopulationMale1July','TPopulationFemale1July','PopSexRatio','Births','SRB','CBR']]
+    # SAMPLE SELECTION
+    # Selecting years from 1950 to 2022
+    Main_df = Main_df[(Main_df['Time'] >= 1950) & (Main_df['Time'] <= 2022)]
+    # Selecting only limited countries for now
+    selected_countries = ["Asia", "World", "South America", "Europe", "Africa" ,"Oceania" , "Australia", "India", "United States of America", "Brazil","United Kingdom" ]
+    new_df = Main_df[Main_df['Location'].isin(selected_countries)].copy()
+    new_df.reset_index(drop=True, inplace=True)
+    new_df['Location'] = new_df['Location'].str.lower()
+
+    # FEATURE EXTRACTION
+    # Deriving values of births in gender
+    def birthgenders(srb, births):
+        denom = (srb/100)+1
+        births_f = births/denom
+        births_m = births-births_f
+        return births_m,births_f
+
+    country_name = country_name.lower()
+
+    # Creating new columns for total male and female births and storing the values after deriving
+    new_df[['BirthM', 'BirthF']] = new_df.apply(lambda row: pd.Series(birthgenders(row['SRB'], row['Births'])), axis=1)
+    cols = list(new_df.columns)
+    cols.insert(7, cols.pop(cols.index('BirthM')))
+    cols.insert(8, cols.pop(cols.index('BirthF')))
+    new_df = new_df.reindex(columns=cols)
+
+    # Creating a Dataframe for the Input of a Country
+    country_wanted = new_df[new_df['Location'] == country_name]
+    country_wanted
+
+    # Selecting the Dependent and Indepnedent Attributes for the Model
+    x_cols_drop = ['Location','TPopulation1July','TPopulationMale1July','TPopulationFemale1July','PopSexRatio','Births','BirthM','BirthF','SRB','CBR']
+    y_cols_drop = ['Location','Time','TPopulation1July','PopSexRatio','Births','SRB','CBR']
+
+    x = country_wanted.drop(columns=x_cols_drop)
+    y = country_wanted.drop(columns=y_cols_drop)
+
+    # Creating Training and Testing Attributes for the Model
+    x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=100)
+
+    # FEATURE SCALING 
+    scaler = StandardScaler()
+    scaler.fit(x_train)
+    x_train_scaled = scaler.transform(x_train)
+    x_test_scaled = scaler.transform(x_test)
+
+    x_train_scaled = pd.DataFrame(x_train_scaled, columns = x_train.columns)
+    x_test_scaled = pd.DataFrame(x_test_scaled, columns = x_test.columns)
+
+    # Using Linear Regression Model and Applying Hyperparameter Tuning Using GridSearchCV
+    fit_intercept = [True,False]
+    copy_X = [True,False]
+    n_jobs = [-1,1]
+    positive = [True,False]
+
+    param_grid = {
+        'fit_intercept': fit_intercept,
+        'copy_X': copy_X,
+        'n_jobs': n_jobs,
+        'positive': positive
+    }
+
+    lr=LinearRegression()
+    grid_search=GridSearchCV(estimator=lr,param_grid=param_grid,cv=10,n_jobs=-1)
+    grid_search.fit(x_train_scaled,y_train)
+
+    best_grid=grid_search.best_estimator_
+
+    model =best_grid.fit(x_train_scaled,y_train)
+
+    # Converting the input year into Dataframe of years from current year to input year
+
+    year_range = range(2023,year+1)
+
+    input_years = np.array(year_range)
+
+    x_pred = pd.DataFrame({'Time': input_years})
+
+    scaler2 = StandardScaler()
+    scaler2.fit(x_test)
+    x_pred_scaled = scaler2.transform(x_pred)
+
+    # Pridicting The Dependent Values
+    predict_test = model.predict(x_pred_scaled)
+
+    # Extracting the Other Attributes Using Predicted Dependent Values of the given Country name and Year
+    pred_MPop = np.round(predict_test[:,0],3)
+    pred_FPop = np.round(predict_test[:,1],3)
+    pred_TPop = np.round(predict_test[:,0] + predict_test[:,1],3)
+    pred_PopSR = np.round((predict_test[:,0] / predict_test[:,1])*100,3)
+    pred_MBirth = np.round(predict_test[:,2],3)
+    pred_FBirth = np.round(predict_test[:,3],3)
+    pred_Tbirths = np.round(predict_test[:,2] + predict_test[:,3],3)
+    pred_SRB = np.round((predict_test[:,2] / predict_test[:,3])*100,3)
+    pred_CBR = np.round((pred_Tbirths / pred_TPop)*1000,3)
+
+    # Predicted Values are Stored in the DataFrame
+    predicted_values = {
+        'Location' : country_name,
+        'Time' : input_years,
+        'Total Population' : pred_TPop,
+        'Male Population' : pred_MPop,
+        'Female Population' : pred_FPop,
+        'Population Gender Ratio' : pred_PopSR,
+        'Total Births' : pred_Tbirths,
+        'Male Births' : pred_MBirth,
+        'Female Births' : pred_FBirth,
+        'Birth Gender Ratio' : pred_SRB,
+        'Birth Rate' : pred_CBR
+    }
+    predicted_df = pd.DataFrame(predicted_values) # this dataframe contains the all predicted values
+    predicted_df['Time'] = predicted_df['Time'].astype(str)
+    predicted_df = predicted_df.set_index('Time')
+    return(predicted_df)
